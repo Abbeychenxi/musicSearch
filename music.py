003b6c1db2a27d8a41d5f7acb903da720d60a9c1
陈希 <abbeychenx@gmail.com>
2015-01-15 23:14:05 +0800

    fix
    增加模糊搜索

--- music.py
+++ music.py
@@ -16,9 +16,25 @@ def query():
         query_words = request.form['query']
         q_acc = ms.parser.parse(query_words)
         res_acc = ms.searcher.search(q_acc)
-        filename = res_acc[0]['title']
-        filename = filename[:-4] + '.mp3'
-        return render_template('index.html', filename=filename)
+        if res_acc:
+            filename = res_acc[0]['title']
+            filename = filename[:-4] + '.mp3'
+            return render_template('index.html', filename=filename)
+        else:
+            cx = {}
+            s = ms.analyzer(query_words)
+            for t in s:
+                q = ms.parser.parse(t.text)
+                results = ms.searcher.search(q)
+                for res in results:
+                    if cx.has_key(res['title']):
+                        cx[res['title']] += 1
+                    else:
+                        cx[res['title']] = 1
+            cx = sorted(cx.iteritems(), key=lambda d:d[1], reverse = True)
+            filename = cx[0][0]
+            filename = filename[:-4] + '.mp3'
+            return render_template('index.html', filename=filename)
     else:
         render_template('index.html', filename=u'朴树-平凡之路.mp3')
 
